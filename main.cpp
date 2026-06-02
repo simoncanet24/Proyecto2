07e65e6ad70a999ade06b231cf6786f1dd9286f0
simon <simon.canet.porras@est.una.ac.cr>
2026-06-01 20:20:40 -0600

    Primer Commit - Creación de Proyecto, repaso de temas y division de trabajo :)

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+
+    /* ----------------------------------------------
+     * no es un juego grafico
+     * simulacion
+     * estado del mundo cambia durante ejecuccion.
+     * archivos de salida
+     * puede ser cualquier tematica
+     * cada vez que se corra el programa tiene que tirar
+     * algo distinto.
+     * ----------------------------------------------
+     * Temas de Progra 2 (no hay que usarlos todos):
+     *
+     * OOP
+     * clases - herencia polimorfismo
+     * punteros
+     * programacion generica (templates e interfaces)
+     * iteradores (no se)
+     * sobrecarga de operadores, excepciones
+     * manejo de archivos
+     * patrones de diseño
+     * -----------------------------------------------
+     * QUE HAY QUE HACER
+     *
+     *
+     *
+     */
+
+    return 0;
+}
